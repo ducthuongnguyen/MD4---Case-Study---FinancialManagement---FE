e28d1d79dfaeda13dbd0e23a610882c6dab7b202
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-06-24 08:25:10 +0700

    thuong day fe

--- frontend/category/category.js
+++ frontend/category/category.js
@@ -21,21 +21,25 @@ function showAllCategory() {
     });
 }
 
+let editId = 0;
+
 function showEditForm(id) {
+    editId = id;
     $('#exampleModal').modal('show');
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/categories/" + id,
         success: function (category) {
+            $("#category-type").val(category.parentCategory.id);
             $("#category-name").val(category.name);
         },
         error: function (error) {
-            error
+            console.log(error)
         }
     });
 }
 
-function getCategory() {
+function getCategoryType() {
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/parent-categories",
@@ -44,17 +48,88 @@ function getCategory() {
             for (let i = 0; i < data.length; i++) {
                 str += `<option value="${data[i].id}">${data[i].name}</option>`
             }
-            $("#category-type").html(str);
+            $(".category-type").html(str);
+        },
+        error: function (error) {
+            console.log(error)
         }
     })
 }
 
-function editCategory() {
+function updateCategory(id) {
+    id = editId;
+    let name = document.getElementById("category-name").value;
+    let type = document.getElementById("category-type").value;
+
+    let category = {
+        name: name,
+        parentCategory: {
+            id: type
+        }
+    }
     $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
         type: "PUT",
         url: "http://localhost:8080/categories?id=" + id,
+        data: JSON.stringify(category),
+        success: function () {
+            showAllCategory()
+        },
+        error: function (error) {
+            console.log(error)
+        }
     })
 }
 
-getCategory()
+// function getCategoryName() {
+//     $.ajax({
+//         type: "GET",
+//         url: "http://localhost:8080/categories",
+//         success: function (data) {
+//             console.log("goi ham cat")
+//             console.log(data)
+//             let str = "";
+//             for (let i = 0; i < data.length; i++) {
+//                 str += `<option value="${data[i].id}">${data[i].name}</option>`
+//             }
+//             $("#name").html(str);
+//         },
+//         error: function (error) {
+//             console.log(error)
+//         }
+//     })
+// }
+
+function showCreateForm() {
+    $('#create').modal('show');
+}
+
+function createCategory() {
+    let name = document.getElementById("name").value;
+    let type = document.getElementById("type").value;
+    let newCategory = {
+        name: name,
+        parentCategory: {
+            id: type
+        }
+    };
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        url: "http://localhost:8080/categories",
+        data: JSON.stringify(newCategory),
+        success: showAllCategory(),
+        error: function (error) {
+            console.log(error)
+        }
+    });
+}
+
+getCategoryType()
 showAllCategory()
