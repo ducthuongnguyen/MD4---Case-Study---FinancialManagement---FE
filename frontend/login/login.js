3d04e491baf2135a556bcacdd961233e48e5aca3
unknown <manh87835@gmail.com>
2022-06-27 15:31:31 +0700

    manh

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,87 +1,104 @@
 
+function logIn() {
+    let logInForm = {};
 
-    $(document).ready(function () {
-    let signInForm = {};
-    $('#btn-login').click(function () {
-    signInForm.email = $('#email').val();
-    signInForm.password = $('#password').val();
-    let signInFormOBJ = JSON.stringify(signInForm);
-    console.log('signInFormOBJ == ', signInFormOBJ)
+    logInForm.username = $('#namelogin').val();
+    logInForm.password = $('#passwordlogin').val();
+    let logInFormOBJ = JSON.stringify(logInForm);
+    console.log('signInFormOBJ == ', logInFormOBJ)
     $.ajax({
-    async: false,
-    url: 'http://localhost:8080/users/logIn',
-    method: 'POST',
-    data: signInFormOBJ,
-    contentType: 'application/json; charset=utf8',
-    success: function (data) {
-    console.log('data login ===> ', data)
-    console.log('data.status', data.status)
-    if (data.status == 202) {
-    document.getElementById('status_login').innerHTML = 'Login Failed! Please try again!';
-    return
+        async: false,
+        url: 'http://localhost:8081/users/signin',
+        method: 'POST',
+        data: logInFormOBJ,
+        contentType: 'application/json; charset=utf8',
+        success: function (data) {
+            console.log('data login ===> ', data)
+            console.log('data.status', data.status)
+            if (data.status == 202) {
+                console.log("ID=",JSON.stringify(data.id))
+                document.getElementById('status_login').innerHTML = 'Login Failed! Please try again!';
+                return
+            }
+            window.sessionStorage.removeItem('TOKEN_KEY');
+
+            window.sessionStorage.setItem('TOKEN_KEY', data.token);
+            window.sessionStorage.removeItem('NAME_KEY');
+            window.sessionStorage.setItem('NAME_KEY', data.name);
+            window.sessionStorage.removeItem('AVATAR_KEY');
+            window.sessionStorage.setItem('AVATAR_KEY', data.avatar);
+            window.sessionStorage.setItem('IDUSER_KEY', data.id);
+            console.log(",,," ,data.id)
+            window.sessionStorage.setItem('ROLE_KEY', JSON.stringify(data.roles))
+
+            window.location.href = '../home/index.html';
+        }
+
+    })
 }
-    window.sessionStorage.removeItem('TOKEN_KEY');
-    window.sessionStorage.setItem('TOKEN_KEY', data.token);
-    window.sessionStorage.removeItem('NAME_KEY');
-    window.sessionStorage.setItem('NAME_KEY', data.name);
-    window.sessionStorage.removeItem('AVATAR_KEY');
-    window.sessionStorage.setItem('AVATAR_KEY', data.avatar);
-    window.sessionStorage.setItem('ROLE_KEY', JSON.stringify(data.roles))
-    console.log("type==", typeof (JSON.stringify(data.roles)))
-    window.location.href = 'hello.html';
-}
 
-})
-})
-})
-
-
-    $(document).ready(function () {
+function signUp() {
     let signUpForm = {};
     let noUser = {
-    message: "no_user"
-}
-    let noEmail = {
-    message: "no_email"
-}
+        message: "user_existed"
+    }
     let createSuccess = {
-    message: "successfully"
-}
-    let password = {
-    message: "password fail"
-}
-
-    $('#btn-create-account').click(function () {
+        message: "yes"
+    }
     signUpForm.name = $('#name').val();
-    signUpForm.email = $('#newEmail').val();
-    signUpForm.password = $('#newPassword').val();
-    signUpForm.passwordConfirm = $('#passwordConfirm').val();
+    signUpForm.username = $('#username').val();
+    signUpForm.email = $('#email').val();
+    signUpForm.password = $('#password').val();
+    signUpForm.roles = ["user"];
     let signUpFormOBJ = JSON.stringify(signUpForm);
     console.log('signUpFormOBJ === ', signUpFormOBJ)
     $.ajax({
-    url: 'http://localhost:8080/users/signUp',
-    method: 'POST',
-    data: signUpFormOBJ,
-    contentType: 'application/json; charset=utf8',
-    success: function (data) {
-    console.log('data ===', data);
-    if (JSON.stringify(data.password) == JSON.stringify(password) && data.status==400) {
-    document.getElementById("passwordStatus").innerHTML = 'PasswordConfirm did not match with Password!'
+        url: 'http://localhost:8081/users/signup',
+        method: 'POST',
+        data: signUpFormOBJ,
+        contentType: 'application/json; charset=utf8',
+        success: function (data) {
+            console.log('data ===', data);
+            if (JSON.stringify(data) == JSON.stringify(noUser)) {
+                document.getElementById('status').innerHTML = 'The username is existed! Please try again!'
+            }
+            if (JSON.stringify(data) == JSON.stringify(createSuccess)) {
+                document.getElementById('status').innerHTML = 'Create User Account Success!'
+            }
+        }
+    })
 }
-    if (JSON.stringify(data) == JSON.stringify(noUser) && data.status==400) {
-    document.getElementById("nameStatus").innerHTML = 'The username is existed! Please try again!'
+function changePassword(){
+    let token=window.sessionStorage.getItem("TOKEN_KEY");
+    console.log("a==",token)
+    let no={
+        message:"no"
+    }
+    let yes={
+        message:"yes"
+    }
+    let changePasswordForm={};
+    changePasswordForm.currentPassword=$('#currentPassword').val();
+    changePasswordForm.newPassword=$('#newPassword').val();
+    let changePassword=JSON.stringify(changePasswordForm);
+    console.log("changPASSform=",changePasswordForm)
+    $.ajax({
+        headers:{
+            Authorization: 'Bearer ' + token
+        },
+        url: 'http://localhost:8081/users/change-password',
+        method: 'PUT',
+        data: changePassword,
+        contentType: 'application/json; charset=utf8',
+        success: function (data) {
+            console.log('data ===', data);
+            if (JSON.stringify(data) == JSON.stringify(no)) {
+                console.log("failed")
+                document.getElementById('status').innerHTML = 'Failed to change password!'
+            }
+            if (JSON.stringify(data) == JSON.stringify(yes)) {console.log("success")
+                document.getElementById('status').innerHTML = 'Change password successfully!'
+            }
+        }
+    })
 }
-    if (JSON.stringify(data) == JSON.stringify(noEmail) && data.status==400) {
-    document.getElementById("emailStatus").innerHTML = 'The Email is existed! Please try again!'
-}
-    if (JSON.stringify(data) == JSON.stringify(createSuccess) ) {
-    console.log("??")
-    status.innerHTML = 'Create User Account Success!'
-}
-}, error: err => {
-    console.log(err)
-}
-})
-})
-})
-
